b0465103324fa8cd18d6a37f94222854b30ff58e
Stefan Bürk <stefan@buerk.tech>
2023-05-07 19:35:36 +0200

    [TASK] Introduce acceptance testing
    This change introduces acceptance testing setup. One simple
    acceptance cest is copied from TYPO3 core to provide a test.
    
    Used command(s):
    
    ```shell
    composer require --dev --no-update \
        "codeception/codeception":"^5.0.10" \
        "codeception/lib-asserts":"^2.1.0" \
        "codeception/module-asserts":"^3.0.0" \
        "codeception/module-cli":"^2.0.1" \
        "codeception/module-filesystem":"^3.0.0" \
        "codeception/module-webdriver":"^4.0.0" \
        "typo3/cms-adminpanel":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-backend":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-beuser":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-belog":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-core":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-dashboard":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-extbase":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-extensionmanager":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-felogin":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-filelist":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-fluid":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-fluid-styled-content":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-form":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-frontend":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-install":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-info":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-indexed-search":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-lowlevel":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-recycler":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-reports":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-seo":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-setup":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-scheduler":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-sys-note":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-tstemplate":"~12.4@dev || ~13.0@dev" \
        "typo3/cms-workspaces":"~12.4@dev || ~13.0@dev"
    ```

---
+++ Tests/Acceptance/Support/Extension/ApplicationEnvironment.php
@@ -0,0 +1,152 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace SBUERK\TF8TestExtension\Tests\Acceptance\Support\Extension;
+
+use Codeception\Event\SuiteEvent;
+use Psr\Http\Message\ServerRequestInterface;
+use Symfony\Component\Mailer\Transport\NullTransport;
+use TYPO3\CMS\Core\Authentication\BackendUserAuthentication;
+use TYPO3\CMS\Core\Core\Bootstrap;
+use TYPO3\CMS\Core\Http\NormalizedParams;
+use TYPO3\CMS\Core\Http\ServerRequest;
+use TYPO3\TestingFramework\Composer\ComposerPackageManager;
+use TYPO3\TestingFramework\Core\Acceptance\Extension\BackendEnvironment;
+
+/**
+ * Load various core extensions and styleguide and call styleguide generator
+ */
+final class ApplicationEnvironment extends BackendEnvironment
+{
+    /**
+     * Load a list of core extensions and styleguide
+     *
+     * @var array
+     */
+    protected $localConfig = [
+        'coreExtensionsToLoad' => [
+            'core',
+            'beuser',
+            'extbase',
+            'fluid',
+            'filelist',
+            'extensionmanager',
+            'setup',
+            'backend',
+            'belog',
+            'install',
+            'frontend',
+            'reports',
+            'sys_note',
+            'scheduler',
+            'tstemplate',
+            'lowlevel',
+            'dashboard',
+            'workspaces',
+            'info',
+            'fluid_styled_content',
+            'indexed_search',
+            'adminpanel',
+            'form',
+            'felogin',
+            'seo',
+            'recycler',
+        ],
+        'testExtensionsToLoad' => [
+            'typo3conf/ext/tf8_extension',
+        ],
+        'csvDatabaseFixtures' => [
+            __DIR__ . '/../../Fixtures/BackendEnvironment.csv',
+        ],
+        'configurationToUseInTestInstance' => [
+            'MAIL' => [
+                'transport' => NullTransport::class,
+            ],
+            'SYS' => [
+                'features' => [
+                    'security.backend.enforceContentSecurityPolicy' => true,
+                ],
+            ],
+        ],
+    ];
+
+    /**
+     * Generate styleguide data
+     */
+    public function bootstrapTypo3Environment(SuiteEvent $suiteEvent): void
+    {
+        parent::bootstrapTypo3Environment($suiteEvent);
+        // styleguide generator uses DataHandler for some parts. DataHandler needs an initialized BE user
+        // with admin right and the live workspace.
+        $request = $this->createServerRequest('https://typo3-testing.local/typo3/');
+        Bootstrap::initializeBackendUser(BackendUserAuthentication::class, $request);
+        $GLOBALS['BE_USER']->user['username'] = 'acceptanceTestSetup';
+        $GLOBALS['BE_USER']->user['admin'] = 1;
+        $GLOBALS['BE_USER']->user['uid'] = 1;
+        $GLOBALS['BE_USER']->workspace = 0;
+        Bootstrap::initializeLanguageObject();
+
+        $composerPackageManager = new ComposerPackageManager();
+
+        // Create favicon.ico to suppress potential javascript errors in console
+        // which are caused by calling a non html in the browser, e.g. seo sitemap xml
+        $faviconTargetPath = '../../../../favicon.ico';
+        $faviconSourcePath = $composerPackageManager->getPackageInfo('backend')->getRealPath()
+            . '/Resources/Public/Icons/favicon.ico';
+        if (!is_file($faviconTargetPath)) {
+            symlink($faviconSourcePath, $faviconTargetPath);
+        }
+    }
+
+    // @todo Eventually move this up to TF::BackendEnvironment, but then as protected.
+    private function createServerRequest(string $url, string $method = 'GET'): ServerRequestInterface
+    {
+        $requestUrlParts = parse_url($url);
+        $docRoot = getenv('TYPO3_PATH_APP') ?? '';
+        $serverParams = [
+            'DOCUMENT_ROOT' => $docRoot,
+            'HTTP_USER_AGENT' => 'TYPO3 Functional Test Request',
+            'HTTP_HOST' => $requestUrlParts['host'] ?? 'localhost',
+            'SERVER_NAME' => $requestUrlParts['host'] ?? 'localhost',
+            'SERVER_ADDR' => '127.0.0.1',
+            'REMOTE_ADDR' => '127.0.0.1',
+            'SCRIPT_NAME' => '/typo3/index.php',
+            'PHP_SELF' => '/typo3/index.php',
+            'SCRIPT_FILENAME' => $docRoot . '/index.php',
+            'QUERY_STRING' => $requestUrlParts['query'] ?? '',
+            'REQUEST_URI' => $requestUrlParts['path'] . (isset($requestUrlParts['query']) ? '?' . $requestUrlParts['query'] : ''),
+            'REQUEST_METHOD' => $method,
+        ];
+        // Define HTTPS and server port
+        if (isset($requestUrlParts['scheme'])) {
+            if ($requestUrlParts['scheme'] === 'https') {
+                $serverParams['HTTPS'] = 'on';
+                $serverParams['SERVER_PORT'] = '443';
+            } else {
+                $serverParams['SERVER_PORT'] = '80';
+            }
+        }
+
+        // Define a port if used in the URL
+        if (isset($requestUrlParts['port'])) {
+            $serverParams['SERVER_PORT'] = $requestUrlParts['port'];
+        }
+        // set up normalizedParams
+        $request = new ServerRequest($url, $method, null, [], $serverParams);
+        return $request->withAttribute('normalizedParams', NormalizedParams::createFromRequest($request));
+    }
+}
---
+++ Tests/Acceptance/Support/Helper/PageTree.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace SBUERK\TF8TestExtension\Tests\Acceptance\Support\Helper;
+
+use Facebook\WebDriver\WebDriverKeys;
+use SBUERK\TF8TestExtension\Tests\Acceptance\Support\ApplicationTester;
+use TYPO3\TestingFramework\Core\Acceptance\Helper\AbstractPageTree;
+
+final class PageTree extends AbstractPageTree
+{
+    private Mouse $mouse;
+
+    /**
+     * Inject our core AcceptanceTester actor into PageTree
+     */
+    public function __construct(ApplicationTester $I, Mouse $mouse)
+    {
+        $this->tester = $I;
+        $this->mouse = $mouse;
+    }
+
+    /**
+     * Perform drag and drop for a new page into the given target page.
+     */
+    public function dragAndDropNewPage(string $pageName, string $dragNode, string $nodeEditInput): void
+    {
+        $target = $this->getPageXPathByPageName($pageName);
+        $pageTitle = sprintf('Dummy 1-%s-new', $pageName);
+
+        $this->mouse->dragAndDrop($dragNode, $target);
+
+        $this->tester->seeElement($nodeEditInput);
+
+        // Change the new page title.
+        // We can't use $I->fillField() here since this sends a clear() to the element
+        // which drops the node creation in the tree. So we do it manually with selenium.
+        $element = $this->tester->executeInSelenium(static function (\Facebook\WebDriver\Remote\RemoteWebDriver $webdriver) use ($nodeEditInput) {
+            return $webdriver->findElement(\Facebook\WebDriver\WebDriverBy::cssSelector($nodeEditInput));
+        });
+        $element->sendKeys($pageTitle);
+
+        $this->tester->pressKey($nodeEditInput, WebDriverKeys::ENTER);
+        $this->tester->waitForElementNotVisible($nodeEditInput);
+        $this->tester->waitForText($pageTitle);
+    }
+
+    /**
+     * Get node identifier of given page.
+     */
+    public function getPageXPathByPageName(string $pageName): string
+    {
+        return '//*[text()=\'' . $pageName . '\']';
+    }
+}
